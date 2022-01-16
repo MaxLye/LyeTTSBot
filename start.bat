fa315359908b26c82446710f24d125ed3fe398c1
MaxLye <MaxLye93@gmail.com>
2022-01-16 09:07:47 +0800

    update key

--- start.bat
+++ start.bat
@@ -1,8 +1,7 @@
-@echo off
 git pull
 pip3 install -r ./requirements.txt
 set SPOTIPY_CLIENT_ID='dc6d6e7a310444f3bf9adcf2449b515b'
-set SPOTIPY_CLIENT_SECRET='3e59f2e9c87945478755881cfa327535'
-set SPOTIPY_REDIRECT_URI='https://adminer.ahlye.com/'
+set SPOTIPY_CLIENT_SECRET='e3aa3ab58a5045038295b1d98db9af92'
+set SPOTIPY_REDIRECT_URI='https://md.ahlye.com/'
 python3 ./telegramNew.py
 pause
