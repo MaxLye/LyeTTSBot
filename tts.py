dbb3a8ae332a4bb6b0a3219a61e76926458a600b
MaxLye93 <MaxLye93@gmail.com>
2022-08-18 19:21:05 -0700

    updated required file and startup file

--- tts.py
+++ tts.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 import sys
 from gtts import gTTS
-from pygame import mixer
-import tempfile
+#from pygame import mixer
+from playsound import playsound
 
 def speak(sentence, cb=None):
-    with tempfile.NamedTemporaryFile(delete=True) as fp:
         try:
             tts=gTTS(text=sentence, lang='zh-TW')
-            tts.save('{}.mp3'.format(fp.name))
-            mixer.init()
-            mixer.music.load('{}.mp3'.format(fp.name))
-            mixer.music.play(1)
-            while mixer.music.get_busy():
-                pass
-            if cb != None:
-                cb()
+            tts.save('./temp.mp3')
+            #mixer.init()
+            #mixer.music.load('{}.mp3'.format(fp.name))
+            #mixer.music.play(1)
+            #while mixer.music.get_busy():
+            #    pass
+            #if cb != None:
+            #    cb()
+            playsound('./temp.mp3')
+            cb()
         except Exception as e:
             print(f"TTS Error : {e}")
             cb()
